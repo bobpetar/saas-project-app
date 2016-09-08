dcd204f27099fd03bc284b22e99adf0ec45d07ca
Petar Risteski <bobpetar@gmail.com>
2016-09-08 11:38:32 +0000

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,8 @@ module ApplicationHelper
   def tenant_name(tenant_id)
     Tenant.find(tenant_id).name
   end
+  
+  def s3_link
+    link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
+  end
 end
