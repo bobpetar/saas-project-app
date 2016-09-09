90f2f54e3e4ab5d5a11d2b1f2f6ddbffd656a66d
Petar Risteski <bobpetar@gmail.com>
2016-09-09 22:25:52 +0000

    add plan, change plan, stripe, javascript

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,10 +1,12 @@
 class Tenant < ActiveRecord::Base
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
   has_many :projects, dependent: :destroy
   validates_uniqueness_of :name
   validates_presence_of :name
+  has_one :payment
+  accepts_nested_attributes_for :payment
   
     def can_create_project
       (plan == 'free' && projects.count < 1) || (plan == 'premium')
